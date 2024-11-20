9ba8048312fad93b5248369993fc398ade92640d
sundas58822 <58822@students.riphah.edu.pk>
2024-11-20 10:07:14 +0500

    Add files via upload

---
+++ Lab 10 task 2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=10;
+	cout<<&a<<endl;
+	int *ptr;
+	ptr=&a;
+	cout<<ptr<<endl;
+	cout<<*ptr<<endl;
+	*ptr=100;
+	cout<<ptr<<endl;
+	cout<<*ptr<<endl;
+	return 0;
+}
+
